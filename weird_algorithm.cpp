9ef3ee41783bb7cf0d417290c7b2a40783c452a4
shubhamkumar755 <shubhamkr77555@gmail.com>
2025-11-02 17:03:12 +0530

    weird algorithm

--- weird_algorithm.cpp
+++ weird_algorithm.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
-    
+    long long n;
+    cin>> n;
+    while(n!=1)
+    {
+        cout<<n<<" ";
+        if(n%2)
+            n=n*3+1;
+        else
+            n/=2;
+    }
+    cout<<n;
 }
